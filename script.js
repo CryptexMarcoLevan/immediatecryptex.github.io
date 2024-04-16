9c0cf0b23feec2d8df1aed0898f8557f2625b273
CryptexMarcoLevan <166449223+CryptexMarcoLevan@users.noreply.github.com>
2024-04-16 12:05:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ document.addEventListener("DOMContentLoaded", function() {
   navbar.style.marginTop = "0";
   navbar.style.paddingTop = "0";
 });
+
+   <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4861353,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4861353&101" alt="webtraffic analytics" border="0"></a></noscript>
+<!-- Histats.com  END  -->
